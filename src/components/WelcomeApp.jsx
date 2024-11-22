e0d4fa8807c48232898e55dfd7f577d580ef1ccd
Siddhartha Khanal <sidkhanal77@gmail.com>
2024-11-22 09:04:09 +0545

    Implemented cannto add more if the length is greater than 5

--- src/components/WelcomeApp.jsx
+++ src/components/WelcomeApp.jsx
@@ -6,31 +6,41 @@ export default function WelcomeApp() {
     const [newName, setNewName] = useState("")
 
     const addNewName = () => {
-        if (newName.trim() !== "" && !names.includes(newName)) {
+        if (newName.trim() !== "" && !names.includes(newName) && names.length < 5) {
             setNames([...names, newName])
             setNewName("")
         }
     }
 
     const removeName = (nameToRemove) => {
-
-
         const names1 = names.filter((name) => name !== nameToRemove)
-        console.log("helo")
         setNames(names1);
     };
 
+    let nameLength = names.length
+    console.log(nameLength)
 
     return (
         <>
+            {names.length}
             <ul>
                 {names.map((name, index) => (
                     <WelcomeList index={index} name={name} onRemove={() => removeName(name)} />
                 ))}
             </ul>
 
-            <input name="" value={newName} onChange={(e) => setNewName(e.target.value)}></input>
-            <button onClick={addNewName}>Add Name </button>
+
+
+
+            {names.length < 5 && (
+                <div>
+                    <input name="" value={newName} onChange={(e) => setNewName(e.target.value)}></input>
+                    <button onClick={addNewName}>Add Name </button>
+                </div>
+
+            )}
+            {names.length >= 5 && <p>Cannot Add More</p>}
+
         </>
     )
 }
